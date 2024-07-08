5f3d20994ecad2b640964111c7652c4f6aff64d9
leeyangie <160584076+leeyangie@users.noreply.github.com>
2024-07-08 10:57:52 +0900

    vo클래스생성-연정
    movie, movieShcedule, screen, theater vo 클래스 생성

--- cineHY/src/main/java/com/hy/myapp/movie/model/vo/Movie.java
+++ cineHY/src/main/java/com/hy/myapp/movie/model/vo/Movie.java
@@ -1,7 +1,26 @@
 package com.hy.myapp.movie.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
 	private String movieCode;
 	private String movieTitle;
 	private String genre;
+	private String summer;
+	private String director;
+	private String actor;
+	private String runningTime;
+	private String rating;
+	private String posterURL;
+	private String openedDate;
+	
 }
--- cineHY/src/main/java/com/hy/myapp/theater/model/vo/Theater.java
+++ cineHY/src/main/java/com/hy/myapp/theater/model/vo/Theater.java
@@ -1,5 +1,11 @@
 package com.hy.myapp.theater.model.vo;
 
 public class Theater {
+	
+	private String theaterCode;
+	private String theaterName;
+	private String theaterAddr;
+	private int locationX;
+	private int locationY;
 
 }
