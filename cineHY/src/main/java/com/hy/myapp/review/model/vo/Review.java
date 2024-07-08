02db6c2ed043a02eac8d6d3984ed8e6cdf3f549e
김민아 <110553201+Kimmingle@users.noreply.github.com>
2024-07-08 14:34:04 +0900

    reviewVO 추가

--- cineHY/src/main/java/com/hy/myapp/review/model/vo/Review.java
+++ cineHY/src/main/java/com/hy/myapp/review/model/vo/Review.java
@@ -3,5 +3,11 @@ package com.hy.myapp.review.model.vo;
 public class Review {
 	
 	private int reviewNo;  //작업함~
+	private String reviewContent;
+	private String reviewDate;
+	private int star;
+	private String status;
+	private String userId;
+	private String movieCd;
 
 }
