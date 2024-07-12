80acd8f91e40684721010ea4beb0d5869e6e8e79
leeyangie <prumpy11@naver.com>
2024-07-13 00:02:46 +0900

    등록된 스케줄 전체조회
    등록된 스케줄 전체조회

---
+++ cineHY/src/main/java/com/hy/myapp/movieSchedule/model/vo/MovieScheduleVO.java
@@ -0,0 +1,28 @@
+package com.hy.myapp.movieSchedule.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class MovieScheduleVO {
+	
+	private String movieTitle;
+	private int screeningId;
+	private String theaterCode;
+	private int screenCode;
+	private String startTime;
+	private String endTime;
+	private String startDate;
+	private String endDate;
+	private int movieCode;
+
+}
